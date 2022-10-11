068f460c2efe5dd685104ead3d6f9f1d667edac2
masan894 <MasanaMail762@gmail.com>
2022-10-11 10:50:14 +0900

    update 1-1,2-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,15 +1,15 @@
 function setup() {
-  createCanvas(128,128);
+  createCanvas(128, 128);
 }
 
 function draw() {
   background(0);
   strokeWeight(4);
   fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
+  triangle(12, 120, 116, 120, 116, 10);
   fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
-  fill(255);
+  triangle(0, 108, 106, 108, 106, 0);
+  fill(119, 136, 153);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  createCanvas(400,400);
+  fill(255);
+  for(let i = 200; i >= 20; i -= 20){
     // BLANK[1]
+  if(i >= 120){
+    stroke(255, 0, 0);
+    ellipse(200, 200, i);
   }
+  else{
+    stroke(0, 0, 255);
+    ellipse(200, 200, i);
+  }
+}
 }
